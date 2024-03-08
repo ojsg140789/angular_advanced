1a6a3e1161629069137ff44394531e0c2e134d03
Omar Soto <ojsg140789@gmail.com>
2024-03-08 15:58:02 -0600

    feat: Redirect login and logout

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunctions(): void;
+
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -12,7 +15,7 @@ export class PagesComponent implements OnInit {
   constructor( private settingsService: SettingsService){}
 
   ngOnInit() {
-    
+    customInitFunctions();
   }
 
 }
